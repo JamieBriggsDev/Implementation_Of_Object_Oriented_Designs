bfe772be4cfb5ddacadab15032a0383854082288
Jamie Briggs <jamie.briggs@hotmail.co.uk>
2018-12-18 21:36:47 +0000

    0.0.1a - Implemented basic interfaces.

---
+++ Assignment/Model/Client.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Client
+    {
+        /// <summary>
+        /// Name of the client.
+        /// </summary>
+        private string m_name;
+        /// <summary>
+        /// Clients ID number.
+        /// </summary>
+        private int m_ClientID;
+
+        /// <summary>
+        /// Name of the client.
+        /// </summary>
+        public string Name
+        {
+            get { return m_name; }
+            set { m_name = value; }
+        }
+
+        /// <summary>
+        /// Clients ID number.
+        /// </summary>
+        public int ClientID
+        {
+            get { return m_ClientID; }
+            set { m_ClientID = value; }
+        }
+        
+    }
+}
---
+++ Assignment/Model/IRegisteredClientsModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public interface IRegisteredClientsModel
+    {
+        /// <summary>
+        /// Get all clients.
+        /// </summary>
+        /// <returns>Returns a list of clients.</returns>
+        List<Client> GetClients();
+
+        /// <summary>
+        /// Register a new client.
+        /// </summary>
+        /// <param name="_entry">New Client.</param>
+        /// <returns></returns>
+        bool Register(Client _entry);
+    }
+}
