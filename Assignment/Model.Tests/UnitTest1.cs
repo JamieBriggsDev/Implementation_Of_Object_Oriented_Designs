3918f62ca1632088938b7532c82ea5f06658c34f
Brandon Taylor <brandontaylor1995@hotmail.com>
2018-12-29 20:49:41 +0000

    0.0.9.3a Added model test project.

---
+++ Assignment/Model.Tests/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model;
+
+namespace Model.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        DatabaseController m_db;
+        [TestInitialize]
+        public void Init()
+        {
+            m_db = new DatabaseController();
+        }
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Job testJob = new Job
+            {
+                FaultDescription = "Testing the faulty stuff.",
+                Open = true,
+                Urgency = 5,
+                Attachment = "www.google.com"
+            };
+
+            Assert.IsTrue(m_db.RegisterJob(testJob), "Failed to register new Job.");
+        }
+    }
+}
--- Assignment/Model/DatabaseController.cs
+++ Assignment/Model/DatabaseController.cs
@@ -47,16 +47,30 @@ namespace Model
             throw new NotImplementedException("Assign staff to job has not been implemented yet.");
         }
 
-        // TODO - BRANDON: Close job
-        public bool CloseJob(int JobID)
+        // Sets the specified job status to closed.
+        public void CloseJob(int jobID)
         {
-            throw new NotImplementedException("Close job has not been implemented yet.");
+            using (var db = new DatabaseContext())
+            {
+                var job = db.Jobs.SingleOrDefault(j => j.JobID == jobID);
+
+                if (job != null)
+                {
+                    job.Open = false;
+                    db.SaveChanges();
+                }
+            }
         }
 
         // TODO - BRANDON: Register a new job
         public bool RegisterJob(Job job)
         {
-            throw new NotImplementedException("Register job has not been implemented yet.");
+            using (var db = new DatabaseContext())
+            {
+                db.Jobs.Add(job);
+            }
+
+            return true;
         }
 
         // TODO - BRANDON: Edit job entrys state
@@ -100,18 +114,17 @@ namespace Model
         // TODO - BRANDON: Add client
         public bool AddClient(string clientName)
         {
-            throw new NotImplementedException("Add client has not been implemented yet.");
+            return true;
         }
 
-        // TODO - BRANDON: Get staff ID
+        // Returns the staff ID of specified staff member.
         public int GetStaffID(string forename, string surname)
         {
             int staffID;
 
             using (var db = new DatabaseContext())
             {
-                var staff = db.Stafflist.Where(s => s.Forename == forename && s.Surname == surname)
-                .First();
+                var staff = db.Stafflist.SingleOrDefault(s => s.Forename == forename && s.Surname == surname);
                 staffID = staff.StaffID;
             }
 
@@ -125,9 +138,10 @@ namespace Model
     public partial class DatabaseContext : DbContext
     {
         public DbSet<Job> Jobs { get; set; }
-        public DbSet<Machine> Machines { get; set; }
+       // public DbSet<Machine> Machines { get; set; }
         public DbSet<Staff> Stafflist { get; set; }
         public DbSet<Address> Addresses { get; set; }
         public DbSet<Client> Clients { get; set; }
+        
     }
 }
